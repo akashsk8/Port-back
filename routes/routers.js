06ec5e592ee865ff7d7c4f81d64777ba2c6887b7
JUMMAN SK <92851033+akashsk8@users.noreply.github.com>
2025-10-29 14:31:01 +0530

    Update again routers.js

--- routes/routers.js
+++ routes/routers.js
@@ -98,7 +98,7 @@ router.post('/Send', async (req,res)=>{
   try{
     const {Name,Email,Message} = req.body;
     await Contact(Name,Email,Message);
-    res.redirect('http://https://port-front-ebon.vercel.app/contact');
+    res.redirect('http://https://jumman.vercel.app/contact');
   }
   catch(error){
     res.status(500).json({ error: 'Failed to send contact message' });
